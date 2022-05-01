fe89665f2414c511dddbf4c27c1c5c620bdf10d5
Lidiya Morshchinina <lmorshchinka@gmail.com>
2022-05-01 22:58:02 +0300

    feat: add new tasks- hanoi, chain, transform

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
+function getCommonCharacterCount(s1, s2) {
   throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  // let result = 0
+  // let newS1 = new Set([...s1])
+  // let newS2 = new Set([...s2])
+  // for (let i = 0; i < newS1.length; i++) {
+  //   if( newS2.includes(newS1[i])){
+  //     result++
+  //   }
+  //   // for (let j = 0; j < s2.length; j++){
+  //   //   if(s1[i] === s2[j]){
+  //   //     result++
+  //   //   }
+  //   // }
+  // }
+  // return result
+  
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!")
+  }
+
+  let result = []
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== '--discard-next' && arr[i] !== '--discard-prev'
+      && arr[i] !== '--double-next' && arr[i] !== '--double-prev') {
+      result.push(arr[i])
+    } else {
+      if (arr[i] === '--discard-next' && arr[i + 1] !== undefined) {
+        i++
+      }
+      if (arr[i] === '--discard-prev' && arr[i - 1] !== undefined && arr[i - 2] !== '--discard-next') {
+        result.pop()
+      }
+      if (arr[i] === '--double-next' && arr[i + 1] !== undefined) {
+        result.push(arr[i + 1])
+      }
+      if (arr[i] === '--double-prev' && arr[i - 1] !== undefined && arr[i - 2] !== '--discard-next') {
+        result.push(arr[i - 1])
+      }
+    }
+  }
+  return result
 }
 
 module.exports = {
